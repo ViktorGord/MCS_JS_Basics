8f5fa42dff578942445698def1a4a04fc18c85b8
ViktorGordikov <viktor.gordikov@yandex.ru>
2021-06-22 16:33:28 +0300

    22.06.2021 (DJ Rashad)
    done

---
+++ DJ/js/script.js
@@ -0,0 +1,11 @@
+let Bodycolr = document.querySelector ("body");
+let Pagecolr = document.querySelector (".page");
+let DJtext = document.querySelector (".name");
+let Art = document.querySelector ("img");
+Bodycolr.style.backgroundColor = "#696969";
+Pagecolr.style.color = "ghostwhite";
+DJtext.innerHTML = "DJ Rashad";
+Art.setAttribute ("src", "img/DJ_Rashad.jpg");
+let Parag = document.querySelector (".short-bio");
+Parag.className += " animated";
+
